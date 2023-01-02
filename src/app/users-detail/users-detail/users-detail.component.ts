7aea78266cb2c460bae1f919d22ccd399af4bbcb
AgustinaInsua <aguss.insua99@gmail.com>
2023-01-02 10:36:00 -0300

    table export to Excel

--- src/app/users-detail/users-detail/users-detail.component.ts
+++ src/app/users-detail/users-detail/users-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { USERS } from 'src/app/model/mock-users';
 import { User } from 'src/app/model/User';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-users-detail',
@@ -15,4 +16,21 @@ export class UsersDetailComponent implements OnInit {
   ngOnInit(): void {
   }
 
+exportExcel() {
+     import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.users);
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "users");
+    });
+}
+
+saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+        type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+}
 }
